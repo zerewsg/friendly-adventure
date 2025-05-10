365ac5804039f7e22e80422281b75873f6b03dcf
zerewsg <ger62076309kyc@gmail.com>
2025-05-10 21:54:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// var userName = prompt("Пожалуйста, введите ваше имя:");
+// alert("Привет, " + userName + "!");
+
+// var userInput = prompt("Введите любое значение:");
+// alert("Тип введенного значения: " + typeof userInput);
+
+
+
+
+// var firstNumber = Number(prompt("Введите первое число:"));
+// var secondNumber = Number(prompt("Введите второе число:"));
+// var sum = firstNumber + secondNumber;
+// alert("Сумма чисел: " + sum);
+
+// var stringInput = prompt("Введите строку для преобразования в число:");
+// var convertedNumber = Number(stringInput);
+// alert("Результат преобразования: " + convertedNumber);
+
+
+
+
+// var numberInput = prompt("Введите число для преобразования в BigInt:");
+// var bigIntValue = BigInt(numberInput);
+// alert("Результат преобразования: " + bigIntValue.toString());
+
+// var firstBigInt = BigInt(prompt("Введите первое большое число:"));
+// var secondBigInt = BigInt(prompt("Введите второе большое число:"));
+// var product = firstBigInt * secondBigInt;
+// alert("Результат умножения: " + product.toString());
